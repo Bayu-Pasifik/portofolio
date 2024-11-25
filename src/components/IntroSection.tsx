2452d373326360ae857fcb6633ed0176fa24c005
milky_way <shin.kagami13@gmail.com>
2024-11-25 21:21:45 +0700

    fix: fixing font in introsection

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,16 +1,33 @@
 "use client";
+
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { Montserrat, Poppins } from "next/font/google";
+
+// Import font Inter dengan pengaturan bobot yang diperlukan
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap", // Mengoptimalkan loading font
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap", // Mengoptimalkan loading font
+});
 
 const IntroSection: React.FC = () => {
   return (
     <section
       id="intro"
-      className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-500 via-white to-blue-50"
+      className={`flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-500 via-white to-blue-50 px-4`}
     >
-      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold text-gray-800 tracking-tight leading-tight">
+      <h1
+        className={` ${montserrat.className} text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold text-gray-800 tracking-tight leading-tight text-center`}
+      >
         Halo, Saya{" "}
-        <span className="text-blue-600">
+        <span className={`text-blue-600 inline sm:inline-block`}>
           <Typewriter
             options={{
               strings: ["Bayu Pasifik"],
@@ -22,12 +39,12 @@ const IntroSection: React.FC = () => {
           />
         </span>
       </h1>
-      <h2 className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-600 max-w-xl text-center">
-        Seorang{" "}
-        <span className="font-semibold text-blue-600">
+      <h2 className={` ${poppins.className} mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-600 max-w-4xlxl text-center`}>
+        Seorang {""}
+        <span className="font-semibold text-blue-600 inline-block">
           <Typewriter
             options={{
-              strings: ["Website Developer", "Mobile Developer"],
+              strings: ["Web Developer", "Mobile Developer"],
               autoStart: true,
               loop: true,
               delay: 75,
@@ -35,7 +52,8 @@ const IntroSection: React.FC = () => {
             }}
           />
         </span>{" "}
-        yang berfokus pada pengembangan website & aplikasi mobile.
+        yang berfokus pada pengembangan{" "}
+        <span className="text-blue-600">website dan aplikasi mobile</span>.
       </h2>
     </section>
   );
