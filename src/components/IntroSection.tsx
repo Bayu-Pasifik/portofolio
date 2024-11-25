2289205914c2d04d8c1e10f27de7ad57014e6610
milky_way <shin.kagami13@gmail.com>
2024-11-25 22:41:15 +0700

    feat: add lottie animation in intro section

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -3,26 +3,28 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import { Montserrat, Poppins } from "next/font/google";
+import Lottie from "lottie-react";
+import scrollDown from "../../public/images/lottie/scroll-down.json";
 
-// Import font Inter dengan pengaturan bobot yang diperlukan
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
-  display: "swap", // Mengoptimalkan loading font
+  display: "swap",
 });
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
-  display: "swap", // Mengoptimalkan loading font
+  display: "swap",
 });
 
 const IntroSection: React.FC = () => {
   return (
     <section
       id="intro"
-      className={`flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-500 via-white to-blue-50 px-4`}
+      className={`relative flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-500 via-white to-blue-50 px-4`}
     >
+      {/* Heading H1 */}
       <h1
         className={` ${montserrat.className} text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold text-gray-800 tracking-tight leading-tight text-center`}
       >
@@ -39,8 +41,12 @@ const IntroSection: React.FC = () => {
           />
         </span>
       </h1>
-      <h2 className={` ${poppins.className} mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-600 max-w-4xlxl text-center`}>
-        Seorang {""}
+
+      {/* Heading H2 */}
+      <h2
+        className={` ${poppins.className} mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-600 w-full text-center`}
+      >
+        Seorang{" "}
         <span className="font-semibold text-blue-600 inline-block">
           <Typewriter
             options={{
@@ -55,8 +61,14 @@ const IntroSection: React.FC = () => {
         yang berfokus pada pengembangan{" "}
         <span className="text-blue-600">website dan aplikasi mobile</span>.
       </h2>
+
+      {/* Lottie Scroll Down */}
+      <div className="absolute bottom-4 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14">
+        <Lottie animationData={scrollDown} loop={true} />
+      </div>
     </section>
   );
 };
 
 export default IntroSection;
+
