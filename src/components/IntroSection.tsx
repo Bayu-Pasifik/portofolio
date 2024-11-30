1a731329b6c0757031a21e7de36ec7ac4df433b8
milky_way <shin.kagami13@gmail.com>
2024-11-30 23:22:34 +0700

    feat: create download CV button

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -5,6 +5,7 @@ import Typewriter from "typewriter-effect";
 import { Montserrat, Poppins } from "next/font/google";
 import Lottie from "lottie-react";
 import scrollDown from "../../public/images/lottie/scroll-down.json";
+import arrowDown from "../../public/images/lottie/arrow-down.json";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -62,6 +63,19 @@ const IntroSection: React.FC = () => {
         <span className="text-blue-600">website dan aplikasi mobile</span>.
       </h2>
 
+      {/* Download CV Button */}
+      <div className="mt-8 flex items-center">
+        <button
+          className="flex items-center px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-full shadow-md hover:bg-blue-700 transition-all focus:outline-none focus:ring-2 focus:ring-blue-400"
+          onClick={() => window.open("/files/CV-Bayu.pdf", "_blank")}
+        >
+          Download CV
+          <div className="w-6 h-6 ml-2">
+            <Lottie animationData={arrowDown} loop={true} />
+          </div>
+        </button>
+      </div>
+
       {/* Lottie Scroll Down */}
       <div className="absolute bottom-4 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14">
         <Lottie animationData={scrollDown} loop={true} />
@@ -71,4 +85,3 @@ const IntroSection: React.FC = () => {
 };
 
 export default IntroSection;
-
